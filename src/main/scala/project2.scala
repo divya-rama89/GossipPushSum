4c60c7b257130b0d5093ff165e873dbb69d2fa90
divya-rama89 <divya.rama89@gmail.com>
2014-10-07 03:24:08 +0530

    Update project2.scala

--- src/main/scala/project2.scala
+++ src/main/scala/project2.scala
@@ -23,16 +23,12 @@ object project2 {
     val topology = (args(1))
     val algorithm = (args(2))  
     
-    // val output: Int = x.getOrElse(-1)
-    
-   // val root = ConfigFactory.load()
-   // val one  = root.getConfig("superBossSystem")
-   // val two  = root.getConfig("bossSystem")
+    val two  = ConfigFactory.load().getConfig("super-boss")
     
     println("args are "+ numNodes+" "+topology+" "+algorithm);
 
       /*Modified by Anirudh Subramanian Begin*/
-      val system = ActorSystem("super-boss")
+      val system = ActorSystem("super-boss", two)
       /*Modified by Anirudh Subramanian End*/
 
      
@@ -44,59 +40,6 @@ object project2 {
       superbossservice ! setupNodes(numNodes, "Yo!", topology, algorithm)
 
 
-      //superbossservice ! executeAlgo(algorithm)
+       }
 
-      //val serverbossservice = system.actorOf(MinerBoss.props(system))
-//      import system.dispatcher
-//      system.scheduler.scheduleOnce(120000 milliseconds, superbossservice, "STOP")
-      
-      // Making the server work
-//      val bossservice = system.actorOf(MinerBoss.props(system)) 
-//      bossservice ! connectTo("super-boss", one.getString("akka.remote.netty.tcp.hostname"))
-      
-          
-  
-  /*    
-      //remoting code here
-      /*
-      val system = ActorSystem("boss-system", ConfigFactory.parseString("""
-    akka {
-       actor {
-          provider = "akka.remote.RemoteActorRefProvider"
-             }
-       remote {
-           transport = ["akka.remote.netty.tcp"]
-       netty.tcp {
-           hostname = "127.0.0.1"
-           port = 2259
-                 }
-             }
-        }
-                                                                        """))
-      */
-      
-      /*Modified by   Anirudh Subramanian Begin*/
-      val system = ActorSystem("boss-system", two)
-      /*Modified by Anirudh Subramanian End*/
-      val bossservice = system.actorOf(MinerBoss.props(system))
-     
-      bossservice ! connectTo("super-boss", args(0))
-
-  
-    /*
-    val system = ActorSystem("miner-system")
-    val minerService =
-      system.actorOf(props(system), "minerboss")
-    */
- */
-  }
-
-  /*
-  def toInt(s: String):Option[Int] = {
-    try {
-      Some(s.toInt)
-    } catch {
-      case e:Exception => None
-    }
-  } */
 }
